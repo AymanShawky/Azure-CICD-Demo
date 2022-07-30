e4562a76c5dcfe8e339f2e402c59c10b178492e5
Ayman Shawky <Ayman.Shawky@integrant.com>
2022-07-31 00:23:13 +0200

    Add project files.

---
+++ DemoCICD.Test/UnitTest1.cs
@@ -0,0 +1,43 @@
+using DemoCICD.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoCICD.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetMethod_ShouldReturnHelloWorld()
+        {
+            HelloController helloController = new HelloController();
+
+            var result = helloController.Get();
+
+            Assert.AreEqual("Hello World!", result);
+        }
+
+        [TestMethod]
+        public void GetMethodWithId_Null_ShouldReturnError()
+        {
+            HelloController helloController = new HelloController();
+
+            var result = helloController.Get("");
+
+            Assert.AreEqual("Error", result);
+        }
+
+        [TestMethod]
+        public void GetMethodWithId_Value_ShouldReturnHelloValue()
+        {
+            //setup
+
+            string value = "Test";
+            
+            HelloController helloController = new HelloController();
+
+            var result = helloController.Get(value);
+
+            Assert.IsTrue(result.Contains(value));
+        }
+    }
+}
---
+++ DemoCICD/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCICD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+
+        [HttpGet("id")]
+        public string Get(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return "Error";
+            }
+            return "Hello " + id;
+        }
+    }
+}
